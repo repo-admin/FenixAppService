b8cec5a3eacb86144fb409024ffd70bd481d3f9e
Vlastimil Vajnorák <vlastimil.vajnorak@creasoft.cz>
2018-08-18 11:40:59 +0200

    test zmena

--- ActionResult.cs
+++ ActionResult.cs
@@ -2,16 +2,16 @@
 
 namespace Fenix
 {
-	/// <summary>Výsledek akce</summary>
-	[DataContract(Namespace = Bc.AppNamespace)]
-	public class ActionResult
-	{
-		/// <summary>ID statusu: 0 - nezdar, 1 - úspěch, 2 - neplatný token, 3 - neoprávněný přístup</summary>
-		[DataMember]
-		public short StatusId { set; get; }
+    /// <summary>Výsledek akce</summary>
+    [DataContract(Namespace = Bc.AppNamespace)]
+    public class ActionResult
+    {
+        /// <summary>Popis statusu</summary>
+        [DataMember]
+        public string StatusDesc { set; get; }
 
-		/// <summary>Popis statusu</summary>
-		[DataMember]
-		public string StatusDesc { set; get; }
-	}
+        /// <summary>ID statusu: 0 - nezdar, 1 - úspěch, 2 - neplatný token, 3 - neoprávněný přístup</summary>
+        [DataMember]
+        public short StatusId { set; get; }
+    }
 }
